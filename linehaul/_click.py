95f93b6c56b5bac25421b571c60c736101783afc
Donald Stufft <donald@stufft.io>
2016-01-13 13:45:23 -0500

    Refactor cleanup to only give 15 seconds for tasks to finish

--- linehaul/_click.py
+++ linehaul/_click.py
@@ -18,7 +18,7 @@ import inspect
 import click
 
 
-async def cleanup(loop):
+async def cleanup(loop, *, timeout=None, cancel=False):
     current_task = asyncio.Task.current_task(loop=loop)
     tasks = [
         t for t in asyncio.Task.all_tasks(loop=loop)
@@ -26,7 +26,11 @@ async def cleanup(loop):
     ]
 
     if tasks:
-        await asyncio.wait(tasks)
+        if cancel:
+            for task in tasks:
+                task.cancel()
+
+        await asyncio.wait(tasks, timeout=timeout, loop=loop)
 
 
 class AsyncCommand(click.Command):
@@ -44,21 +48,47 @@ class AsyncCommand(click.Command):
             @functools.wraps(original_callback)
             def wrapper(*args, **kwargs):
                 loop = asyncio.get_event_loop()
-                main_t = loop.create_task(original_callback(*args, **kwargs))
+                main_t = asyncio.ensure_future(
+                    original_callback(*args, **kwargs),
+                    loop=loop,
+                )
 
                 try:
-                    loop.run_forever()
-                except KeyboardInterrupt:
-                    click.echo(click.style("Shutting Down...", fg="yellow"))
-
-                    main_t.cancel()
                     try:
-                        loop.run_until_complete(cleanup(loop))
+                        loop.run_until_complete(main_t)
                     except KeyboardInterrupt:
-                        tasks = [t for t in asyncio.Task.all_tasks(loop=loop)]
-                        for task in tasks:
-                            task.cancel()
+                        main_t.cancel()
+                        # This won't actually run forever because the call to
+                        # loop.run_until_complete added a callback to the
+                        # future that will stop the loop once main_t has
+                        # finished and return control back to this function.
                         loop.run_forever()
+
+                    # Try to clean up all of the tasks by waiting for any
+                    # existing tasks to finish. Ideally the main function
+                    # triggered everything to try and finish up and exit on
+                    # it's own. However, if it hadn't then we'll cancel
+                    # everything after we wait a small amount of time.
+                    cleanup_t = asyncio.ensure_future(
+                        cleanup(loop, timeout=15),
+                        loop=loop,
+                    )
+                    try:
+                        loop.run_until_complete(cleanup_t)
+                    except KeyboardInterrupt:
+                        # We got another KeyboardInterrupt while waiting on the
+                        # pending tasks to finish. We'll cancel that cleanup
+                        # job and let everything fall through to the final
+                        # cleanup that just cancels everything.
+                        cleanup_t.cancel()
+                        # Like above, this will not actually run forever
+                        # because of callback added to the cleanup_t task.
+                        loop.run_forever()
+                finally:
+                    # Just cancel everything at this point, we don't want
+                    # anything to still be executing once this is over.
+                    loop.run_until_complete(cleanup(loop, cancel=True))
+                    loop.stop()
 
             self.callback = wrapper
 
