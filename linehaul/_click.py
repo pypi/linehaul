3c7425e3d1b011053fff9a5f2a6f8ba0aeb2ca55
Donald Stufft <donald@stufft.io>
2016-01-13 11:18:12 -0500

    Refactor the click integration to better handle cleanup

--- linehaul/_click.py
+++ linehaul/_click.py
@@ -18,16 +18,15 @@ import inspect
 import click
 
 
-async def kill_all(loop, timeout=None):
+async def cleanup(loop):
+    current_task = asyncio.Task.current_task(loop=loop)
     tasks = [
         t for t in asyncio.Task.all_tasks(loop=loop)
-        if t is not asyncio.Task.current_task(loop=loop)
+        if t is not current_task
     ]
 
-    for task in tasks:
-        task.cancel()
-
-    return await asyncio.wait(tasks, timeout=timeout)
+    if tasks:
+        await asyncio.wait(tasks)
 
 
 class AsyncCommand(click.Command):
@@ -45,15 +44,21 @@ class AsyncCommand(click.Command):
             @functools.wraps(original_callback)
             def wrapper(*args, **kwargs):
                 loop = asyncio.get_event_loop()
-                coro = original_callback(*args, **kwargs)
-                future = asyncio.ensure_future(coro, loop=loop)
+                main_t = loop.create_task(original_callback(*args, **kwargs))
 
                 try:
-                    return loop.run_until_complete(future)
+                    loop.run_forever()
                 except KeyboardInterrupt:
-                    future.cancel()
-                    loop.run_until_complete(future)
-                    loop.run_until_complete(kill_all(loop))
+                    click.echo(click.style("Shutting Down...", fg="yellow"))
+
+                    main_t.cancel()
+                    try:
+                        loop.run_until_complete(cleanup(loop))
+                    except KeyboardInterrupt:
+                        tasks = [t for t in asyncio.Task.all_tasks(loop=loop)]
+                        for task in tasks:
+                            task.cancel()
+                        loop.run_forever()
 
             self.callback = wrapper
 
