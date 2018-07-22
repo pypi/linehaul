6b142b487934c27c68406303d9ba71fee4563087
Donald Stufft <donald@stufft.io>
2018-07-22 11:08:40 -0400

    Switch the basic LineReceiver test over to using hypothesis

--- tests/protocol/test_line_receiver.py
+++ tests/protocol/test_line_receiver.py
@@ -10,10 +10,30 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from functools import partial
+
+from hypothesis import given, strategies as st
+
 from linehaul.protocol.line_receiver import LineReceiver
 
+from ..strategies import line_delimited_data as _line_delimited_data, chunked
 
-def test_yields_lines():
-    lr = LineReceiver(lambda line: line)
-    lines = lr.receive_data(b"This is a line.\nAnd this is another line.\n")
-    assert lines == [b"This is a line.\n", b"And this is another line.\n"]
+
+_max_line_size = st.integers(min_value=1, max_value=32768)
+max_line_size = st.shared(_max_line_size, key="max-line-size")
+
+line_delimited_data = partial(
+    _line_delimited_data, max_line_size=st.shared(_max_line_size, key="max-line-size")
+)
+
+
+@given(max_line_size, chunked(line_delimited_data()))
+def test_yields_lines(max_line_size, data):
+    lr = LineReceiver(lambda line: line, max_line_size=max_line_size)
+
+    lines = []
+    for chunk in data:
+        lines.extend(lr.receive_data(chunk))
+    lr.close()
+
+    assert lines == [i + b"\n" for i in b"".join(data).split(b"\n")[:-1]]
---
+++ tests/strategies.py
@@ -0,0 +1,36 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from hypothesis import strategies as st
+
+
+@st.composite
+def line_delimited_data(draw, max_line_size):
+    n = draw(max_line_size)
+    data = st.binary(min_size=1, max_size=n).filter(lambda d: b"\n" not in d)
+    lines = draw(
+        st.lists(data, min_size=1).filter(lambda l: sum(map(len, l)) + len(l) <= n)
+    )
+    return b"\n".join(lines) + b"\n"
+
+
+@st.composite
+def chunked(draw, source):
+    data = draw(source)
+
+    chunk_sizes = [0]
+    chunk_sizes += draw(
+        st.lists(st.integers(0, len(data) - 1), unique=True).map(sorted)
+    )
+    chunk_sizes += [len(data)]
+
+    return [data[u:v] for u, v in zip(chunk_sizes, chunk_sizes[1:])]
