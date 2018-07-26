3af11eab7373a937f8df9858efbc41cdc9cfc940
Donald Stufft <donald@stufft.io>
2018-07-26 17:33:21 -0400

    Package Linehaul

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+install_requires = []
+with open("requirements/main.in", "r") as fp:
+    for line in fp:
+        line.strip()
+        if line:
+            install_requires.append(line)
+
+
+setup(
+    name="linehaul",
+    use_scm_version={
+        "local_scheme": lambda v: "+{.node}{}".format(v, ".dirty" if v.dirty else ""),
+        "version_scheme": lambda v: "3.{.distance}.0".format(v),
+    },
+    packages=find_packages(exclude=["tests*"]),
+    package_data={"linehaul": ["schema.json"]},
+    entry_points={"console_scripts": ["linehaul = linehaul.cli:main"]},
+    install_requires=install_requires,
+    setup_requires=["setuptools_scm"],
+)
