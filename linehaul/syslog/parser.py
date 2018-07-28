1d127a6acd0b57aaad18cefe5e82e3fce397dd72
Donald Stufft <donald@stufft.io>
2018-07-28 00:48:47 -0400

    Properly handle Nil hostnames

--- linehaul/syslog/parser.py
+++ linehaul/syslog/parser.py
@@ -41,7 +41,7 @@ LBRACKET = L("[").suppress()
 RBRACKET = L("]").suppress()
 COLON = L(":").suppress()
 
-NIL = L('"-"')
+NIL = L("-")
 NIL.setName("Nil")
 NIL.setParseAction(lambda s, l, t: NilValue)
 
@@ -54,7 +54,7 @@ TIMESTAMP = Word(printables)
 TIMESTAMP = TIMESTAMP.setResultsName("timestamp")
 TIMESTAMP.setName("Timestamp")
 
-HOSTNAME = Combine(NIL | Word(printables))
+HOSTNAME = NIL | Word(printables)
 HOSTNAME = HOSTNAME.setResultsName("hostname")
 HOSTNAME.setName("Hostname")
 
