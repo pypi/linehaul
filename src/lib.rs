a5de3ac856c54f9f6879f89a95582f170c250cc8
Donald Stufft <donald@stufft.io>
2019-01-28 16:17:16 -0500

    Cleanup Parsing

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,20 @@ use std::str;
 #[macro_use]
 extern crate nom;
 
-use log::{info};
+use log::{error, info};
 
 mod syslog;
 
 pub fn process<'a>(lines: impl Iterator<Item = &'a str>) {
     for line in lines {
-        let message: SyslogMessage = line.parse();
+        let message: syslog::SyslogMessage = match line.parse() {
+            Ok(m) => { m },
+            Err(_e) => {
+                error!("Could not parse '{:?}' as a syslog message.", line);
+                continue;
+             }
+        };
+
         break;
     }
 }
