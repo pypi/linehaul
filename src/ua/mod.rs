1492448853e1a1ea04085de6f146d55cff73164f
Donald Stufft <donald@stufft.io>
2019-01-31 20:10:03 -0500

    Add a small note

--- src/ua/mod.rs
+++ src/ua/mod.rs
@@ -14,6 +14,11 @@ lazy_static! {
 ua_parser!(
     UserAgentParser,
     pip6(r"^pip/(?P<version>\S+)\s+(?P<data>.+)$") => |_version, data| {
+        // TODO: To match the implementation of the Python parser, we would have to
+        //       check that the pip version is >= 6... however that's a bit tricky
+        //       here because Rust doesn't have anything that implements PEP 440. I
+        //       think it might be pointless to do though, because if it's not pip 6+
+        //       then serde will fail to deserialize and we should move onto the next.
         match serde_json::from_str::<UserAgent>(data) {
             Ok(ua) => Some(ua),
             Err(_e) => None,
